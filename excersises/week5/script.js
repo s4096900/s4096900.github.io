cc5405e094beea2c778d91c1e583f46f1d4aa200
s4096900 <s4096900@student.rmit.edu.au>
2025-04-03 12:08:54 +1100

    week 5 work

---
+++ excersises/week5/script.js
@@ -0,0 +1,54 @@
+console.log("hello");
+//
+// let myName = prompt("whats your name?");
+let myName = "hi";
+console.log("hello", myName);
+
+// if a letter is used the quotation marks are needed
+// let id = 1234;
+let id = "s1234";
+console.log("my id is", id);
+
+// let myCity = prompt("whats your city?");
+// console.log("my city is", myCity);
+
+// my-class-students, kebab-case, used for html and css
+// myClassStudents, camelCase every word after the first one has a captital, used for java script
+
+// () used fro functions /conditions
+// [] used for arrays
+// {} used to define scope and used in funtions
+// const is a closed box
+// let is used for changing values
+
+{
+  let a = 10;
+  console.log("a =", a);
+}
+
+//  + addition - subratction * multiplicationn / division
+//string variable text '' "" `` backtick or grave
+let a = 1;
+let b = parseInt("2");
+//pareseInt make the variable a number?
+let c = 3;
+console.log(a, b, c);
+
+// Boolean variable
+let isItSunday = false;
+
+//const
+const myStudent = {
+  name: "abc",
+  id: 123,
+  homeTown: "spain",
+};
+console.log(myStudent);
+
+let coolStuff = `123, ${isItSunday}`;
+console.log(coolStuff);
+
+// arrays position 1 is 0 and each variable after goes up from there
+const id2 = [1, 1, "did", 2, 4, 34, 526, 65];
+console.log(id2);
+console.log(id2[4]);
