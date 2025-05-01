bfff0360a37c37184b7037c93c083f91ec4dc2fd
s4096900 <s4096900@student.rmit.edu.au>
2025-05-01 11:12:57 +1000

    updates to at2 and class work
    adding stuff for week 8 and doing work on at2

---
+++ assignment2/script.js
@@ -0,0 +1,39 @@
+const glyph1 = document.querySelector("#glyph1");
+const glyph2 = document.querySelector("#glyph2");
+const glyph3 = document.querySelector("#glyph3");
+const glyphImages = document.querySelectorAll(".glyph-image");
+const progressBar = document.querySelector("#progress-bar");
+const playPauseButton = document.querySelector("#play-pause-button");
+const playPauseImg = document.querySelector("#play-pause-img");
+const glyph1Button = document.querySelector("#glyph1-button");
+const glyph2Button = document.querySelector("#glyph2-button");
+const glyph3Button = document.querySelector("#glyph3-button");
+const fullscreenButton = document.querySelector("#fullscreen-button");
+const volumeSlider = document.querySelector("#volume-slider");
+
+const imageSources = [
+  "https://cdnb.artstation.com/p/assets/images/images/043/163/227/original/augustin-cart-gif-lofi-final.gif?1636484521",
+  "https://media1.tenor.com/m/ZSRfK14Kek8AAAAd/lofi-vaporwave.gif",
+  "https://i.gifer.com/PPy.gif",
+];
+
+glyph1Button.addEventListener("click", function () {
+  showGlyph(0);
+  currentVideo = 0;
+});
+
+glyph2Button.addEventListener("click", function () {
+  showGlyph(1);
+  currentVideo = 1;
+});
+
+glyph3Button.addEventListener("click", function () {
+  showGlyph(2);
+  currentVideo = 2;
+});
+
+// Modified function to show the selected glyph
+function showGlyph(index) {
+  glyphImages.forEach((img) => (img.style.display = "none")); // Hide all images
+  glyphImages[index].style.display = "block"; // Show the selected image
+}
